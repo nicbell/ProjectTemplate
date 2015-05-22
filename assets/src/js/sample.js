d05633d795e74659bc1c35c1dbf22724967c5ce4
Nic Bell <nic.bell@hogarthww.com>
2015-05-22 17:53:28 +0100

    Sample module

---
+++ assets/src/js/sample.js
@@ -0,0 +1,36 @@
+/*
+	Description: Sample class
+*/
+
+(function (App) {
+
+    App.Sample = klass({
+
+        options: {
+            defaultValue1: null,
+            defaultValue2: null
+        },
+
+        initialize: function (element) {
+            //if using jQuery..
+            //this.$element = $(element);
+            //this.options = $.extend(true, {}, this.options, this.$element.data('sample-options') || {});
+
+            //if native ES6
+            //this.element = element;
+            //this.options = Object.assign(this.options, JSON.parse(this.element.dataset.sampleOptions || {}));
+
+            this.addEvents();
+        },
+
+        addEvents: function () {
+
+        }
+    });
+
+
+    Attach.add('App.Sample', function (el) {
+        el['App.Sample'] = el['App.Sample'] || new App.Sample(el);
+    });
+
+})(window.App = window.App || {});
