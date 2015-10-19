42ce4c35c7b71dc1b2c5a72454c25b579439552e
Nic Bell <nic@ixoxo.co.uk>
2015-10-19 17:07:10 +0100

    Switched to postscss

--- _npmscripts/build-scss.js
+++ _npmscripts/build-scss.js
@@ -1,41 +1,17 @@
 // Requires.
-var fs = require('fs');
-var sass = require('node-sass');
-var postcss = require('postcss');
+var postscss = require('postscss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 var config = require('../package').config;
 
 // Compile sass.
 function compile() {
-	sass.render({
-		file: config.scss.srcDir + config.scss.srcFile,
-		outFile: config.scss.distDir + config.scss.distFile,
-		outputStyle: 'compressed',
-		precision: 10,
-		sourceMap: true,
-	}, function (err, result) {
-		if (!err) {
-			postCSS(result);
-		}
-		else {
-			console.log(err);
-		}
-	});
-}
-
-// Auto Prefixer and CSS nano.
-function postCSS(result) {
 	var prefix = autoprefixer({ browsers: ['> 1%', 'IE 7'] }),
 		nano = cssnano();
 
-	postcss([prefix, nano]).process(result.css, {
-		from: config.scss.distFile,
-		to: config.scss.distFile,
-		map: { inline: false }
-	}).then(function (result) {
-		fs.writeFile(config.scss.distDir + config.scss.distFile, result.css);
-		fs.writeFile(config.scss.distDir + config.scss.sourceMap, result.map);
+	postscss([prefix, nano]).process({
+		from: config.scss.srcDir + config.scss.srcFile,
+		to: config.scss.distDir + config.scss.distFile
 	});
 }
 
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 # Project Template
 
-##My project template for front-end builds.
+## My project template for front-end builds.
 
 This project uses NPM scripts. We only need to run `npm run build` which will automatically run `prebuild` and install everything we need.
 
 To create a watcher we can either run `npm run watch`.
+
+## Adding generated files to .net projects.
+
+```xml
+<Target Name="BeforeBuild">
+<ItemGroup>
+	<Content Include="wwwroot\dist\**\*.*" />
+</ItemGroup>
+</Target>
+```
